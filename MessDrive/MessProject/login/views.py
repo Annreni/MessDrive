f964bd1d61ec91020822787c891efdf8b1da6418
Annreni <107064240+Annreni@users.noreply.github.com>
2022-06-11 12:06:16 +0530

    Add files via upload

---
+++ MessDrive/MessProject/login/views.py
@@ -0,0 +1,8 @@
+import http
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+    #return HttpResponse("Hello World")
